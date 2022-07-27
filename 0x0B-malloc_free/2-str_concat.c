6d5a9aef816db0850c9e2eed2f5d6051890289a1
JayOkeiyi <81185689+JayOkeiyi@users.noreply.github.com>
2022-07-27 02:19:20 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,39 +1,97 @@
 #include "main.h"
 #include <stdlib.h>
 
-/**
- *  * str_concat - Concatenates two strings.
- *   * @s1: The string to be concatenated upon.
- *    * @s2: The string to be concatenated to s1.
- *     * Return: If concatenation fails - NULL.
- *      * Otherwise - a pointer the newly-allocated space in memory
- *       * containing the concatenated strings.
- *        */
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
 
-char *str_concat(char *s1, char *s2)
+/**
+ * word_len - Locates the index marking the end of the
+ * first word contained within a string.
+ * @str: The string to be searched.
+ * Return: The index marking the end of the initial word pointed to by str.
+ */
+
+int word_len(char *str)
+{
+int index = 0, len = 0;
+while (*(str + index) && *(str + index) != ' ')
+{
+len++;
+index++;
+}
+return (len);
+}
+
+/**
+ * count_words - Counts the number of words contained within a string.
+ * @str: The string to be searched.
+ * Return: The number of words contained within str.
+ */
+
+int count_words(char *str)
+{
+int index = 0, words = 0, len = 0;
+for (index = 0; *(str + index); index++)
+len++;
+
+for (index = 0; index < len; index++)
+{
+if (*(str + index) != ' ')
+{
+words++;
+index += word_len(str + index);
+}
+}
+return (words);
+
+}
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The string to be split.
+ * Return: If str = NULL, str = "", or the function fails - NULL.
+ * Otherwise - a pointer to an array of strings (words).
+ */
+
+char **strtow(char *str)
 
 {
-	char *concat_str;
-	int index, concat_index = 0, len = 0;
+char **strings;
+int index = 0, words, w, letters, l;
+if (str == NULL || str[0] == '\0')
+return (NULL);
 
-	if (s1 == NULL)
-		s1 = "";
+words = count_words(str);
+if (words == 0)
+return (NULL);
 
-	if (s2 == NULL)
-		s2 = "";
+strings = malloc(sizeof(char *) * (words + 1));
+if (strings == NULL)
+return (NULL);
 
-	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
-	concat_str = malloc(sizeof(char) * len);
+for (w = 0; w < words; w++)
+{
+while (str[index] == ' ')
+index++;
+letters = word_len(str + index);
+strings[w] = malloc(sizeof(char) * (letters + 1));
 
-	if (concat_str == NULL)
-		return (NULL);
+if (strings[w] == NULL)
+{
+for (; w >= 0; w--)
+free(strings[w]);
 
-	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
+free(strings);
+return (NULL);
+}
 
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
-	return (concat_str);
+for (l = 0; l < letters; l++)
+strings[w][l] = str[index++];
+strings[w][l] = '\0';
+
+}
+strings[w] = NULL;
+return (strings);
 
 }
