177234e4b89dac0bd1df4770ebe030d5dac0b422
JayOkeiyi <81185689+JayOkeiyi@users.noreply.github.com>
2022-07-27 02:19:50 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,45 +1,97 @@
 #include "main.h"
 #include <stdlib.h>
 
-/**
- *  * alloc_grid - Returns a pointer to a 2-dimensional array of
- *   * integers with each element initalized to 0.
- *    * @width: The width of the 2-dimensional array.
- *     * @height: The height of the 2-dimensional array.
- *      * Return: If width <= 0, height <= 0, or the function fails - NULL.
- *       * Otherwise - a pointer to the 2-dimensional array of integers.
- *        */
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
 
-int **alloc_grid(int width, int height)
+/**
+ * word_len - Locates the index marking the end of the
+ * first word contained within a string.
+ * @str: The string to be searched.
+ * Return: The index marking the end of the initial word pointed to by str.
+ */
+
+int word_len(char *str)
+{
+int index = 0, len = 0;
+while (*(str + index) && *(str + index) != ' ')
+{
+len++;
+index++;
+}
+return (len);
+}
+
+/**
+ * count_words - Counts the number of words contained within a string.
+ * @str: The string to be searched.
+ * Return: The number of words contained within str.
+ */
+
+int count_words(char *str)
+{
+int index = 0, words = 0, len = 0;
+for (index = 0; *(str + index); index++)
+len++;
+
+for (index = 0; index < len; index++)
+{
+if (*(str + index) != ' ')
+{
+words++;
+index += word_len(str + index);
+}
+}
+return (words);
+
+}
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The string to be split.
+ * Return: If str = NULL, str = "", or the function fails - NULL.
+ * Otherwise - a pointer to an array of strings (words).
+ */
+
+char **strtow(char *str)
 
 {
+char **strings;
+int index = 0, words, w, letters, l;
+if (str == NULL || str[0] == '\0')
+return (NULL);
 
-	int **twoD;
-	int hgt_index, wid_index;
-	if (width <= 0 || height <= 0)
-		return (NULL);
+words = count_words(str);
+if (words == 0)
+return (NULL);
 
-	twoD = malloc(sizeof(int *) * height);
-	if (twoD == NULL)
-		return (NULL);
+strings = malloc(sizeof(char *) * (words + 1));
+if (strings == NULL)
+return (NULL);
 
-	for (hgt_index = 0; hgt_index < height; hgt_index++)
-	{
-		twoD[hgt_index] = malloc(sizeof(int) * width);
+for (w = 0; w < words; w++)
+{
+while (str[index] == ' ')
+index++;
+letters = word_len(str + index);
+strings[w] = malloc(sizeof(char) * (letters + 1));
 
-		if (twoD[hgt_index] == NULL)
-		{
-			for (; hgt_index >= 0; hgt_index--)
-				free(twoD[hgt_index]);
-			free(twoD);
-			return (NULL);
-		}
-	}
-	for (hgt_index = 0; hgt_index < height; hgt_index++)
-	{
-		for (wid_index = 0; wid_index < width; wid_index++)
-			twoD[hgt_index][wid_index] = 0;
-	}
-	return (twoD);
+if (strings[w] == NULL)
+{
+for (; w >= 0; w--)
+free(strings[w]);
+
+free(strings);
+return (NULL);
+}
+
+for (l = 0; l < letters; l++)
+strings[w][l] = str[index++];
+strings[w][l] = '\0';
+
+}
+strings[w] = NULL;
+return (strings);
 
 }
