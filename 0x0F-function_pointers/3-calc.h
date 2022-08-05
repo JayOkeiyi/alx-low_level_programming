ce639a880589e94873205d13af81260301932315
JayOkeiyi <81185689+JayOkeiyi@users.noreply.github.com>
2022-08-05 14:13:54 +0100

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,27 +1,27 @@
-#ifndef CALC_H
-#define CALC_H
-
-/*
- *  * Desc: Header file containing all structures and
- *   * prototypes used by the 3-main.c program.
- *    */
+#include "function_pointers.h"
 
 /**
- *  * struct op - A struct op.
- *   * @op: The operator.
- *    * @f: The associated function.
- *     */
+ * int_index - Searches for an integer in an array of integers.
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * @cmp: A pointer to the function to be used to compare values.
+ * Return: If no element matches or size <= 0 - -1.
+ * Otherwise - The index of the first element for which
+ * the cmp function does not return 0.
+ */
 
-typedef struct op
+int int_index(int *array, int size, int (*cmp)(int))
+
 {
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+int index;
 
-#endif
+if (array == NULL || cmp == NULL)
+return (-1);
+
+for (index = 0; index < size; index++)
+{
+if (cmp(array[index]) != 0)
+return (index);
+}
+return (-1);
+}
